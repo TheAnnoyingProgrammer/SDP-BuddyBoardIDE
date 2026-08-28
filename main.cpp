20ea581146110c151950fafe4afab015ad55e58f
Mohammad Haque <mahaque.ed@gmail.com>
2026-08-27 20:53:20 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.resize(1280, 800);
     w.show();
-    return QCoreApplication::exec();
+    return a.exec();
 }
